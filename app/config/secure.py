44756ca01777ea1ac4230e9115089fe50373d50d
StephenYuan7 <2583242079@qq.com>
2021-07-01 19:34:58 +0800

    Update secure.py

--- app/config/secure.py
+++ app/config/secure.py
@@ -2,7 +2,8 @@
 这个文件保存一些有关服务器的设置
 """
 SQLALCHEMY_DATABASE_URI = \
-    '数据库地址'
+    'mysql+cymysql://lab_1664251310:bd4ed0ee4651_#@Aa' \
+    '@rm-bp1oo27t8762xhlob0o.mysql.rds.aliyuncs.com:3306/rds_mysql_3206xit'
 
-SECRET_KEY = '加密算法'
+SECRET_KEY = 'ahkjhkfjahkfhkaj'
 
